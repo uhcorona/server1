46a1de7864c1f72400177ab54872804333f55ada
uhcorona <67886042+uhcorona@users.noreply.github.com>
2020-10-03 09:08:09 -0600

    update app

--- api/app.py
+++ api/app.py
@@ -12,13 +12,10 @@ urlb = 'http://3.16.203.164/'
 def postNote():
     cpu1 = requests.get(urla+'cpuusage').json()['usedcpu']
     cpu2 = requests.get(urlb+'cpuusage').json()['usedcpu']
-
     ram1 = requests.get(urla+'ramusage').json()['usedram']
     ram2 = requests.get(urlb+'ramusage').json()['usedram']
-    
     n1 = requests.get(urla+'nnotes').json()['elements']
     n2 = requests.get(urlb+'nnotes').json()['elements']
-
     if n1 > n2 :
         requests.post(urlb + 'notes', data = request.data)
     elif n1 < n2 :
@@ -35,9 +32,10 @@ def postNote():
                 requests.post(urla + 'notes', data = request.data)
             elif cpu1 == cpu2:
                 requests.post(urla + 'notes', data = request.data)
+    return Response(response = "{\"res\": \"true\"}", status=200, mimetype="application/json")
 
-
-
+@app.route('/', methods=['GET'])
+def gget():
     return Response(response = "{\"res\": \"true\"}", status=200, mimetype="application/json")
 
 if __name__ == '__main__':
