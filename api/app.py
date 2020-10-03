b96dd19b3fa682d7b6062234182e608c0dd690bc
uhcorona <67886042+uhcorona@users.noreply.github.com>
2020-10-02 22:55:34 -0600

    server1

---
+++ api/app.py
@@ -0,0 +1,44 @@
+import requests
+
+from flask import Flask, request
+from flask import Response
+
+app = Flask(__name__)
+
+urla = 'http://3.135.187.4/'
+urlb = 'http://3.129.195.85/'
+
+@app.route('/notes', methods=['POST'])
+def postNote():
+    cpu1 = requests.get(urla+'cpuusage').json()['usedcpu']
+    cpu2 = requests.get(urlb+'cpuusage').json()['usedcpu']
+
+    ram1 = requests.get(urla+'ramusage').json()['usedram']
+    ram2 = requests.get(urlb+'ramusage').json()['usedram']
+    
+    n1 = requests.get(urla+'nnotes').json()['elements']
+    n2 = requests.get(urlb+'nnotes').json()['elements']
+
+    if n1 > n2 :
+        requests.post(urlb + 'notes', data = request.data)
+    elif n1 < n2 :
+        requests.post(urla + 'notes', data = request.data)
+    elif n1 == n2:
+        if ram1 > ram2 :
+            requests.post(urlb + 'notes', data = request.data)
+        elif ram1 < ram2 :
+            requests.post(urla + 'notes', data = request.data)
+        elif ram1 == ram2:
+            if cpu1 > cpu2 :
+                requests.post(urlb + 'notes', data = request.data)
+            elif cpu1 < cpu2 :
+                requests.post(urla + 'notes', data = request.data)
+            elif cpu1 == cpu2:
+                requests.post(urla + 'notes', data = request.data)
+
+
+
+    return Response(response = "{\"res\": \"true\"}", status=200, mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port = 4000)
